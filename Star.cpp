4652f7f0f9fac44dd5f03319ab602bcd0982c2a2
Sejalpawar29 <132185983+Sejalpawar29@users.noreply.github.com>
2023-08-05 22:46:21 +0530

    Add files via upload

---
+++ Star.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    for (int i = 5; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
